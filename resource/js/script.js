790946ab666a0d8a7d24572c49cd01ad1d103aaf
Eggie <55740098+lifeofeggie@users.noreply.github.com>
2020-12-17 02:18:11 +0900

    Add files via upload
    201217

--- resource/js/script.js
+++ resource/js/script.js
@@ -12,6 +12,37 @@ $(function(){
             $(".logo img").attr("src","resource/images/logoOriginal.svg");
         }
     });
+    $(window).on("load", function() {
+
+        var $landingWrapper = $(".landing-wrapper"),
+            $landingInnerContent = $(".landing-inner-content");
+
+        // set initial container to half of .landing-inner-content width
+        //TweenMax.set($landingWrapper, {scrollTo: {x: $landingInnerContent.width()/4}, ease: Power2.easeOut});
+
+        // scroll left and right
+        $landingInnerContent.on("mousemove touchmove", function(e) {
+        if (e.clientX > $landingWrapper.width() / 2) {
+            TweenMax.to($landingWrapper, 2, {
+            scrollTo: {
+                x: "+=175"
+            },
+            ease: Power2.easeOut
+            });
+        } else {
+            TweenMax.to($landingWrapper, 2, {
+            scrollTo: {
+                x: "-=175"
+            },
+            ease: Power2.easeOut
+            });
+        }
+        });
+
+        });
+    
+
+
     function getCurrentScroll() {
         return window.pageYOffset || document.documentElement.scrollTop;
     }
